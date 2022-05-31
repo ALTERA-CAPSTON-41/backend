76e5fa3e4d2edf44cd18781aa6e5aa8b10cff5a4
dhichii <adhicittamasran@gmail.com>
2022-05-31 14:13:50 +0800

    feat(patient): implement routes and handler

---
+++ src/handlers/patient_handler.go
@@ -0,0 +1,112 @@
+package handlers
+
+import (
+	"clinic-api/src/models"
+	"clinic-api/src/utils"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+func CreatePatientHandler(c echo.Context) error {
+	status := http.StatusCreated
+	patientRequest := models.Patient{}
+	if err := c.Bind(&patientRequest); err != nil {
+		status = http.StatusBadRequest
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	newPatient := models.MapToNewPatient(patientRequest)
+	if err := models.DB.Create(&newPatient).Error; err != nil {
+		status = http.StatusInternalServerError
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	return utils.CreateEchoResponse(c, status, map[string]uuid.UUID{"id": newPatient.ID})
+}
+
+func GetAllPatientsHandler(c echo.Context) error {
+	status := http.StatusOK
+	name := c.QueryParam("name")
+	nik := c.QueryParam("nik")
+	patients := []models.Patient{}
+
+	query := "UPPER(name) LIKE '%" + strings.ToUpper(name) + "%'"
+	if nik != "" {
+		query = fmt.Sprint("nik = ", nik)
+	}
+
+	err := models.DB.
+		Where(query).
+		Find(&patients).Error
+	if err != nil {
+		status = http.StatusInternalServerError
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	return utils.CreateEchoResponse(c, status, models.MapToPatientBatch(patients))
+}
+
+func GetPatientByIDHandler(c echo.Context) error {
+	status := http.StatusOK
+	id := c.Param("id")
+	patient := models.Patient{}
+	if err := models.DB.Where("id", id).First(&patient).Error; err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			status = http.StatusNotFound
+			return utils.CreateEchoResponse(c, status, nil)
+		}
+
+		status = http.StatusInternalServerError
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	return utils.CreateEchoResponse(c, status, models.MapToPatient(patient))
+}
+
+func EditDoctorByIDHandler(c echo.Context) error {
+	status := http.StatusNoContent
+	id := c.Param("id")
+	patientRequest := models.Patient{}
+	if err := c.Bind(&patientRequest); err != nil {
+		status = http.StatusBadRequest
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	patient := models.MapToExistingPatient(patientRequest)
+	editAction := models.DB.Where("id", id).Updates(&patient)
+	if editAction.RowsAffected < 1 {
+		status = http.StatusNotFound
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	err := editAction.Error
+	if err != nil {
+		status = http.StatusInternalServerError
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	return utils.CreateEchoResponse(c, status, nil)
+}
+
+func DeletePatientByIDHandler(c echo.Context) error {
+	status := http.StatusNoContent
+	id := c.Param("id")
+	patient := models.Patient{}
+	deleteAction := models.DB.Where("id", id).Delete(&patient)
+	if deleteAction.RowsAffected < 1 {
+		status = http.StatusNotFound
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	err := deleteAction.Error
+	if err != nil {
+		status = http.StatusInternalServerError
+		return utils.CreateEchoResponse(c, status, nil)
+	}
+
+	return utils.CreateEchoResponse(c, status, nil)
+}
--- src/routes/route.go
+++ src/routes/route.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"clinic-api/src/handlers"
 	"clinic-api/src/utils"
 	"net/http"
 
@@ -12,10 +13,17 @@ func New() *echo.Echo {
 
 	route.GET("/", func(ec echo.Context) error {
 		status := http.StatusOK
-		return utils.CreateEchoResponse(ec, status, http.StatusText(status), map[string]string{
+		return utils.CreateEchoResponse(ec, status, map[string]string{
 			"data": "server works!",
 		})
 	})
+
+	// patient
+	route.POST("/patients", handlers.CreatePatientHandler)
+	route.GET("/patients", handlers.GetAllPatientsHandler)
+	route.GET("/patients/:id", handlers.GetPatientByIDHandler)
+	route.PUT("/patients/:id", handlers.EditDoctorByIDHandler)
+	route.DELETE("/patients/:id", handlers.DeletePatientByIDHandler)
 
 	return route
 }
