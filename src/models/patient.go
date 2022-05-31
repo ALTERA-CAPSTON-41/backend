1a88ab5c8c49eb0fb2dd72031084dfa9280b1bea
dhichii <adhicittamasran@gmail.com>
2022-05-31 14:10:33 +0800

    feat(patient): add mapping function

--- src/models/patient.go
+++ src/models/patient.go
@@ -7,6 +7,13 @@ import (
 	"gorm.io/gorm"
 )
 
+type genderType string
+
+const (
+	MALE   = "MALE"
+	FEMALE = "FEMALE"
+)
+
 type Patient struct {
 	ID        uuid.UUID      `gorm:"primaryKey" json:"id"`
 	Name      string         `json:"name"`
@@ -14,9 +21,68 @@ type Patient struct {
 	Phone     string         `json:"phone"`
 	Address   string         `json:"address"`
 	DOB       time.Time      `json:"dob"`
-	Gender    string         `json:"gender"`
+	Gender    genderType     `gorm:"type:enum('MALE', 'FEMALE')" json:"gender"`
 	BloodType string         `json:"blood_type"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+}
+
+type PatientResponse struct {
+	ID        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	NIK       string    `json:"nik"`
+	Phone     string    `json:"phone"`
+	Address   string    `json:"address"`
+	DOB       time.Time `json:"dob"`
+	Gender    genderType    `json:"gender"`
+	BloodType string    `json:"blood_type"`
+}
+
+func MapToNewPatient(model Patient) Patient {
+	return Patient{
+		ID:        uuid.Must(uuid.NewRandom()),
+		Name:      model.Name,
+		NIK:       model.NIK,
+		Phone:     model.Phone,
+		Address:   model.Address,
+		DOB:       model.DOB,
+		Gender:    model.Gender,
+		BloodType: model.BloodType,
+	}
+}
+
+func MapToExistingPatient(model Patient) Patient {
+	return Patient{
+		ID:        model.ID,
+		Name:      model.Name,
+		NIK:       model.NIK,
+		Phone:     model.Phone,
+		Address:   model.Address,
+		DOB:       model.DOB,
+		Gender:    model.Gender,
+		BloodType: model.BloodType,
+	}
+}
+
+func MapToPatient(model Patient) PatientResponse {
+	return PatientResponse{
+		ID:        model.ID,
+		Name:      model.Name,
+		NIK:       model.NIK,
+		Phone:     model.Phone,
+		Address:   model.Address,
+		DOB:       model.DOB,
+		Gender:    model.Gender,
+		BloodType: model.BloodType,
+	}
+}
+
+func MapToPatientBatch(model []Patient) []PatientResponse {
+	var patients []PatientResponse
+
+	for _, patient := range model {
+		patients = append(patients, MapToPatient(patient))
+	}
+	return patients
 }
