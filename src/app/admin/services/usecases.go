59e2d2a03316b2de2273cd51cdc4e18cabf09203
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-06-10 06:25:11 +0700

    feat(admin): implement usecases with logics

--- src/app/admin/services/usecases.go
+++ src/app/admin/services/usecases.go
@@ -8,27 +8,27 @@ type usecase struct {
 
 // AmendAdminByID implements admin.Services
 func (uc *usecase) AmendAdminByID(id string, admin admin.Domain) (err error) {
-	panic("unimplemented")
+	return uc.repo.UpdateByID(id, admin)
 }
 
 // CreateAdmin implements admin.Services
 func (uc *usecase) CreateAdmin(admin admin.Domain) (id string, err error) {
-	panic("unimplemented")
+	return uc.repo.InsertData(admin)
 }
 
 // GetAdminByID implements admin.Services
 func (uc *usecase) GetAdminByID(id string) (admin admin.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectDataByID(id)
 }
 
 // GetAllAdmins implements admin.Services
 func (uc *usecase) GetAllAdmins() (admins []admin.Domain, err error) {
-	panic("unimplemented")
+	return uc.repo.SelectAllData()
 }
 
 // RemoveAdminByID implements admin.Services
 func (uc *usecase) RemoveAdminByID(id string) (err error) {
-	panic("unimplemented")
+	return uc.repo.DeleteByID(id)
 }
 
 func NewService(repo admin.Repositories) admin.Services {
