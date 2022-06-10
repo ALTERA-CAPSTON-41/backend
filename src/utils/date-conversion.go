f5309dd0f40fc5edfbd396e22a290374ec8cbf84
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-10 11:44:50 +0800

    perf: add string to datetime conversion

--- src/utils/date-conversion.go
+++ src/utils/date-conversion.go
@@ -10,3 +10,8 @@ func ConvertStringToDate(date string) time.Time {
 	t, _ := time.Parse("2006-01-02", date)
 	return t
 }
+
+func ConvertStringToDatetime(datetime string) time.Time {
+	t, _ := time.Parse("2006-01-02 15:04:05", datetime)
+	return t
+}
