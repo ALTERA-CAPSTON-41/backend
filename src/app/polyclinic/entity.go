aa18d16cdfba9dc631f19fbed301926672232f35
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-13 15:59:11 +0800

    feat(polyclinic): add domain and interfaces

---
+++ src/app/polyclinic/entity.go
@@ -0,0 +1,21 @@
+package polyclinic
+
+type Domain struct {
+	Name string
+}
+
+type Services interface {
+	CreatePolyclinic(Domain) (int, error)
+	GetAllPolyclinics() ([]Domain, error)
+	GetPolyclinicByID(id int) (*Domain, error)
+	AmendPolyclinicByID(id string, polyclinic Domain) error
+	RemovePolyclinicByID(id int) error
+}
+
+type Repositories interface {
+	InsertData(data Domain) (int, error)
+	SelectAllData() ([]Domain, error)
+	SelectDataByID(id int) (*Domain, error)
+	UpdateByID(id int, data Domain) error
+	DeleteByID(id int) error
+}
