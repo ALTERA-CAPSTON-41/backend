4420e8a61c7b9f468353399e520ca34394817045
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-13 16:34:45 +0800

    feat(polyclinic): define model

---
+++ src/app/polyclinic/repositories/record.go
@@ -0,0 +1,30 @@
+package repositories
+
+import (
+	"clinic-api/src/app/polyclinic"
+
+	"gorm.io/gorm"
+)
+
+type Polyclinic struct {
+	ID        int
+	Name      string
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func MapToDomain(record Polyclinic) polyclinic.Domain {
+	return polyclinic.Domain{
+		Name: record.Name,
+	}
+}
+
+func MapToNewRecord(domain polyclinic.Domain) Polyclinic {
+	return Polyclinic{Name: domain.Name}
+}
+
+func MapToExistingRecord(id int, domain polyclinic.Domain) Polyclinic {
+	return Polyclinic{
+		ID:   id,
+		Name: domain.Name,
+	}
+}
