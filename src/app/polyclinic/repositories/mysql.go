f2e804a7e80d3b1e99d1c26f675b7597d7ebbd7e
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-13 21:28:41 +0800

    feat(polyclinic): implement gorm query to repo

--- src/app/polyclinic/repositories/mysql.go
+++ src/app/polyclinic/repositories/mysql.go
@@ -1,7 +1,8 @@
-package repositories
+package polyclinic_repositories
 
 import (
 	"clinic-api/src/app/polyclinic"
+	"errors"
 
 	"gorm.io/gorm"
 )
@@ -10,24 +11,56 @@ type repository struct {
 	DB *gorm.DB
 }
 
+// InserData implements polyclinic.Repositories
 func (repo *repository) InsertData(data polyclinic.Domain) (int, error) {
-	panic("unimplemented")
+	record := MapToNewRecord(data)
+	if err := repo.DB.Create(&record).Error; err != nil {
+		return 0, err
+	}
+
+	return record.ID, nil
 }
 
+// SelectAllData implements polyclinic.Repositories
 func (repo *repository) SelectAllData() ([]polyclinic.Domain, error) {
-	panic("unimplemented")
+	var records []Polyclinic
+	if err := repo.DB.Find(&records).Error; err != nil {
+		return nil, err
+	}
+
+	return MapToBatchDomain(records), nil
 }
 
+// SelectDataByID implements polyclinic.Repositories
 func (repo *repository) SelectDataByID(id int) (*polyclinic.Domain, error) {
-	panic("unimplemented")
+	var record Polyclinic
+	if err := repo.DB.First(&record, id).Error; err != nil {
+		return nil, err
+	}
+
+	polyclinic := MapToDomain(record)
+	return &polyclinic, nil
 }
 
+// UpdateByID implements polyclinic.Repositories
 func (repo *repository) UpdateByID(id int, data polyclinic.Domain) error {
-	panic("unimplemented")
+	record := MapToExistingRecord(id, data)
+	alteration := repo.DB.Where("id", id).Updates(&record)
+	if alteration.RowsAffected < 1 {
+		return errors.New("record not found")
+	}
+
+	return alteration.Error
 }
 
+// DeleteByID implements polyclinic.Repositories
 func (repo *repository) DeleteByID(id int) error {
-	panic("unimplemented")
+	deletion := repo.DB.Delete(new(Polyclinic), id)
+	if deletion.RowsAffected < 1 {
+		return errors.New("record not found")
+	}
+
+	return deletion.Error
 }
 
 func NewMySQLRepository(DB *gorm.DB) polyclinic.Repositories {
