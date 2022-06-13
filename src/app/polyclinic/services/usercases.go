deee63088f7c289713e228b33bec9f9270236791
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-13 21:37:22 +0800

    feat(polyclinic): implement usecases with logics

--- src/app/polyclinic/services/usercases.go
+++ src/app/polyclinic/services/usercases.go
@@ -6,24 +6,29 @@ type usecase struct {
 	repo polyclinic.Repositories
 }
 
+// CreatePolyclinic implements polyclinic.Services
 func (uc *usecase) CreatePolyclinic(polyclinic polyclinic.Domain) (int, error) {
-	panic("unimplemented")
+	return uc.repo.InsertData(polyclinic)
 }
 
+// GetAllPolyclinics implements polyclinic.Services
 func (uc *usecase) GetAllPolyclinics() ([]polyclinic.Domain, error) {
-	panic("unimplemented")
+	return uc.repo.SelectAllData()
 }
 
+// GetPolyclinicByID implements polyclinic.Services
 func (uc *usecase) GetPolyclinicByID(id int) (*polyclinic.Domain, error) {
-	panic("unimplemented")
+	return uc.repo.SelectDataByID(id)
 }
 
+// AmendPolyclinicByID implements polyclinic.Services
 func (uc *usecase) AmendPolyclinicByID(id int, polyclinic polyclinic.Domain) error {
-	panic("unimplemented")
+	return uc.repo.UpdateByID(id, polyclinic)
 }
 
+// RemovePolyclinicByID implements polyclinic.Services
 func (uc *usecase) RemovePolyclinicByID(id int) error {
-	panic("unimplemented")
+	return uc.repo.DeleteByID(id)
 }
 
 func NewService(repo polyclinic.Repositories) polyclinic.Services {
