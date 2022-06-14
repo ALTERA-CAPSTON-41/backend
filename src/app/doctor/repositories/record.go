ce34e75e1410a3506eefb46fa98751a25ec8845c
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-06-14 13:41:45 +0700

    chore(doctor): add record to domain mapper

--- src/app/doctor/repositories/record.go
+++ src/app/doctor/repositories/record.go
@@ -1,7 +1,9 @@
 package repositories
 
 import (
+	"clinic-api/src/app/doctor"
 	"clinic-api/src/types"
+	"clinic-api/src/utils"
 	"time"
 
 	"github.com/google/uuid"
@@ -35,4 +37,77 @@ type User struct {
 	Email    string `gorm:"unique"`
 	Password string
 	Role     types.UserRoleEnum `gorm:"type:enum('DOCTOR', 'ADMIN', 'NURSE')"`
+}
+
+func MapToDomain(record Doctor) doctor.Domain {
+	return doctor.Domain{
+		UserID:  record.User.ID,
+		Name:    record.Name,
+		NIP:     record.NIP,
+		SIP:     record.NIP,
+		Address: record.Address,
+		DOB:     record.DOB,
+		Gender:  record.Gender,
+		Polyclinic: doctor.PolyclinicReference{
+			ID:   record.Polyclinic.ID,
+			Name: record.Polyclinic.Name,
+		},
+		User: doctor.UserReference{
+			ID:        record.User.ID,
+			Email:     record.User.Email,
+			Password:  record.User.Password,
+			Role:      record.User.Role,
+			CreatedAt: record.CreatedAt,
+			UpdatedAt: record.UpdatedAt,
+		},
+	}
+}
+
+func MapToNewRecord(domain doctor.Domain) Doctor {
+	newUserID := uuid.Must(uuid.NewRandom())
+	hashed, _ := utils.CreateHash(domain.User.Password)
+	return Doctor{
+		User: User{
+			ID:       newUserID,
+			Email:    domain.User.Email,
+			Password: hashed,
+			Role:     domain.User.Role,
+		},
+		UserID:       newUserID,
+		Name:         domain.Name,
+		NIP:          domain.NIP,
+		SIP:          domain.SIP,
+		Address:      domain.Address,
+		DOB:          domain.DOB,
+		Gender:       domain.Gender,
+		PolyclinicID: domain.Polyclinic.ID,
+	}
+}
+
+func MapToExistingRecord(domain doctor.Domain) Doctor {
+	return Doctor{
+		UserID:       domain.User.ID,
+		Name:         domain.Name,
+		NIP:          domain.NIP,
+		SIP:          domain.SIP,
+		Address:      domain.Address,
+		DOB:          domain.DOB,
+		Gender:       domain.Gender,
+		PolyclinicID: domain.Polyclinic.ID,
+		User: User{
+			ID:       domain.User.ID,
+			Email:    domain.User.Email,
+			Password: domain.User.Password,
+			Role:     domain.User.Role,
+		},
+	}
+}
+
+func MapToBatchDomain(records []Doctor) []doctor.Domain {
+	var domains []doctor.Domain
+
+	for _, record := range records {
+		domains = append(domains, MapToDomain(record))
+	}
+	return domains
 }
