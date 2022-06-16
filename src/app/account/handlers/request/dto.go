b50f0e4d4b45913e2d5887a53d5eca0fe6bf1107
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-06-16 22:14:44 +0700

    refactor(account): move request to domain mapper to handlers

--- src/app/account/handlers/request/dto.go
+++ src/app/account/handlers/request/dto.go
@@ -1,6 +1,11 @@
 package request
 
-type UserRequest struct {
+import (
+	"clinic-api/src/app/account"
+	"clinic-api/src/types"
+)
+
+type UpdateRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 	Role     string `json:"role"`
@@ -9,4 +14,19 @@ type UserRequest struct {
 type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (req *UpdateRequest) MapToDomain() account.Domain {
+	return account.Domain{
+		Email:    req.Email,
+		Password: req.Password,
+		Role:     types.UserRoleEnum(req.Role),
+	}
+}
+
+func (req *LoginRequest) MapToDomain() account.Domain {
+	return account.Domain{
+		Email:    req.Email,
+		Password: req.Password,
+	}
 }
