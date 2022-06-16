41bf9d1d6a78a43f0b61e18aa1dbc3205afd528d
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-06-17 02:05:26 +0700

    refactor(account): extract business logic from MVC handler

--- src/app/account/services/usecases.go
+++ src/app/account/services/usecases.go
@@ -2,6 +2,10 @@ package services
 
 import (
 	"clinic-api/src/app/account"
+	"clinic-api/src/types"
+	"clinic-api/src/utils"
+	"errors"
+	"fmt"
 )
 
 type usecase struct {
@@ -10,7 +14,29 @@ type usecase struct {
 
 // AttemptLogin implements account.Services
 func (uc *usecase) AttemptLogin(domain account.Domain) (*account.UserDataDomain, error) {
-	panic("unimplemented")
+	var user *account.Domain
+	var err error
+
+	if user, err = uc.repo.LookupAccountByEmail(domain.Email); err != nil {
+		return nil, err
+	}
+
+	if !utils.ValidateHash(domain.Password, user.Password) {
+		fmt.Println("result", domain.Password, user.Password, utils.ValidateHash(domain.Password, user.Password))
+		return nil, errors.New("password not match")
+	}
+
+	var result *account.UserDataDomain
+	if user.Role == types.ADMIN {
+		if result, err = uc.repo.LookupAdminByUserID(user.ID.String()); err != nil {
+			return nil, err
+		}
+	} else if user.Role == types.DOCTOR {
+		if result, err = uc.repo.LookupDoctorByUserID(user.ID.String()); err != nil {
+			return nil, err
+		}
+	}
+	return result, nil
 }
 
 func NewService(repo account.Repositories) account.Services {
