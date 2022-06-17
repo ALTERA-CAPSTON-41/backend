3e69f02afb5a636d42272c0175731dc42edefd4d
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-17 21:56:43 +0800

    feat(nurse): implement gorm query to repo

--- src/app/nurse/repositories/mysql.go
+++ src/app/nurse/repositories/mysql.go
@@ -2,6 +2,7 @@ package nurse_repositories
 
 import (
 	"clinic-api/src/app/nurse"
+	"errors"
 
 	"gorm.io/gorm"
 )
@@ -12,24 +13,47 @@ type repository struct {
 
 // InsertData implements nurse.Repositories
 func (repo *repository) InsertData(data nurse.Domain) (string, error) {
-	panic("unimplemented")
+	newNurse := MapToNewRecord(data)
+	return newNurse.User.ID.String(), repo.DB.Create(&newNurse).Error
 }
 
 // SelectAllData implements nurse.Repositories
 func (repo *repository) SelectAllData() ([]nurse.Domain, error) {
-	panic("unimplemented")
+	var records []Nurse
+	err := repo.DB.Preload("User").Preload("Polyclinic").Find(&records).Error
+	return MapToBatchDomain(records), err
 }
 
+// SelectDataByID implements nurse.Repositories
 func (repo *repository) SelectDataByID(id string) (*nurse.Domain, error) {
-	panic("unimplemented")
+	var record Nurse
+	if err := repo.DB.Preload("User").Preload("Polyclinic").Where("user_id", id).First(&record).Error; err != nil {
+		return nil, err
+	}
+
+	result := MapToDomain(record)
+	return &result, nil
 }
 
+// UpdateByID implements nurse.Repositories
 func (repo *repository) UpdateByID(id string, domain nurse.Domain) error {
-	panic("unimplemented")
+	data := MapToExistingRecord(domain)
+	query := repo.DB.Where("user_id", id).Updates(&data)
+	if query.RowsAffected < 1 && query.Error == nil {
+		return errors.New("record not found")
+	}
+
+	return query.Error
 }
 
+// DeleteByID implements nurse.Repositories
 func (repo *repository) DeleteByID(id string) error {
-	panic("unimplemented")
+	query := repo.DB.Where("user_id", id).Delete(new(Nurse))
+	if query.RowsAffected < 1 && query.Error == nil {
+		return errors.New("record not found")
+	}
+
+	return query.Error
 }
 
 func NewMySQLRepository(DB *gorm.DB) nurse.Repositories {
