217b9b9780498083a0aab8bc3cc9b0baf8fdb5d1
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2022-06-20 16:08:58 +0700

    chore(medical_record): define record and domain mapper

--- src/app/medical_record/repositories/record.go
+++ src/app/medical_record/repositories/record.go
@@ -1,6 +1,7 @@
 package medicalrecord_repositories
 
 import (
+	medicalrecord "clinic-api/src/app/medical_record"
 	"clinic-api/src/types"
 	"time"
 
@@ -43,4 +44,57 @@ type Doctor struct {
 	NIP    string           `gorm:"column:nip"`
 	SIP    string           `gorm:"column:sip"`
 	Gender types.GenderEnum `gorm:"type:enum('MALE', 'FEMALE')"`
+}
+
+func (mr *MedicalRecord) MapToDomain() medicalrecord.Domain {
+	return medicalrecord.Domain{
+		ID:               mr.ID,
+		Symptoms:         mr.Symptoms,
+		ICD10Code:        mr.ICD10Code,
+		ICD10Description: mr.ICD10Description,
+		Suggestions:      mr.Suggestions,
+		Patient: medicalrecord.PatientReference{
+			ID:        mr.Patient.ID,
+			Name:      mr.Patient.Name,
+			NIK:       mr.Patient.NIK,
+			Phone:     mr.Patient.Phone,
+			Address:   mr.Patient.Address,
+			DOB:       mr.Patient.DOB,
+			Gender:    mr.Patient.Gender,
+			BloodType: mr.Patient.BloodType,
+		},
+		Doctor: medicalrecord.DoctorReference{
+			ID:     mr.Doctor.UserID,
+			Name:   mr.Doctor.Name,
+			NIP:    mr.Doctor.NIP,
+			SIP:    mr.Doctor.SIP,
+			Gender: mr.Doctor.Gender,
+		},
+		Polyclinic: medicalrecord.PolyclinicReference{
+			ID:   mr.Polyclinic.ID,
+			Name: mr.Polyclinic.Name,
+		},
+	}
+}
+
+func MapToNewRecord(domain medicalrecord.Domain) MedicalRecord {
+	return MedicalRecord{
+		ID:               uuid.Must(uuid.NewRandom()),
+		Symptoms:         domain.Symptoms,
+		ICD10Code:        domain.ICD10Code,
+		ICD10Description: domain.ICD10Description,
+		Suggestions:      domain.Suggestions,
+		PolyclinicID:     domain.Polyclinic.ID,
+		PatientID:        domain.Patient.ID,
+		DoctorID:         domain.Doctor.ID,
+	}
+}
+
+func MapToBatchDomain(records []MedicalRecord) []medicalrecord.Domain {
+	var domains []medicalrecord.Domain
+
+	for _, record := range records {
+		domains = append(domains, record.MapToDomain())
+	}
+	return domains
 }
