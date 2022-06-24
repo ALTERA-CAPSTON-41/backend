26252476eb16bb3cb4a455cc8e6dadf8dc71e6cf
Adhicitta Masran <adhicittamasran@gmail.com>
2022-06-24 08:11:25 +0800

    feat(dashboard): implement gorm query to repo

---
+++ src/app/dashboard/repositories/mysql.go
@@ -0,0 +1,32 @@
+package dashboard_repositories
+
+import (
+	"clinic-api/src/app/dashboard"
+
+	"gorm.io/gorm"
+)
+
+type repository struct {
+	DB *gorm.DB
+}
+
+// CountData implements dashboard.Repositories
+func (repo *repository) CountData(table string) (int, error) {
+	var total int64
+	err := repo.DB.Table(table).Where("deleted_at IS NULL").Count(&total).Error
+	return int(total), err
+}
+
+// CountQueueData implements dashboard.Repositories
+func (repo *repository) CountQueueData() (int, error) {
+	var total int64
+	err := repo.DB.
+		Table("queues").
+		Where("deleted_at IS NULL AND daily_queue_date = CURDATE()").
+		Count(&total).Error
+	return int(total), err
+}
+
+func NewMySQLRepository(DB *gorm.DB) dashboard.Repositories {
+	return &repository{DB}
+}
