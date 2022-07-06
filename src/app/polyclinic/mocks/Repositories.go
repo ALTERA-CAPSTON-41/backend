5fac4ef7945886fc277198788e26af773c17ee1a
Adhicitta Masran <adhicittamasran@gmail.com>
2022-07-06 23:31:43 +0800

    test(polyclinic): update mockery mocks

--- src/app/polyclinic/mocks/Repositories.go
+++ src/app/polyclinic/mocks/Repositories.go
@@ -15,6 +15,48 @@ type Repositories struct {
 	mock.Mock
 }
 
+// CountDoctorByPolyclinic provides a mock function with given fields: _a0
+func (_m *Repositories) CountDoctorByPolyclinic(_a0 int) (int, error) {
+	ret := _m.Called(_a0)
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func(int) int); ok {
+		r0 = rf(_a0)
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(int) error); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// CountNurseByPolyclinic provides a mock function with given fields: _a0
+func (_m *Repositories) CountNurseByPolyclinic(_a0 int) (int, error) {
+	ret := _m.Called(_a0)
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func(int) int); ok {
+		r0 = rf(_a0)
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(int) error); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // DeleteByID provides a mock function with given fields: id
 func (_m *Repositories) DeleteByID(id int) error {
 	ret := _m.Called(id)
