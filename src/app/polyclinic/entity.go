b6e32baf6e5b250fcf9d5c174bb462a6d6378e4f
Adhicitta Masran <adhicittamasran@gmail.com>
2022-07-06 23:11:54 +0800

    refactor(polyclinic): update domain and interfaces

--- src/app/polyclinic/entity.go
+++ src/app/polyclinic/entity.go
@@ -1,14 +1,17 @@
 package polyclinic
 
 type Domain struct {
-	ID   int
-	Name string
+	ID          int
+	Name        string
+	TotalDoctor int
+	TotalNurse  int
 }
 
 type Services interface {
 	CreatePolyclinic(Domain) (int, error)
 	GetAllPolyclinics() ([]Domain, error)
 	GetPolyclinicByID(id int) (*Domain, error)
+	GetAllPolyclinicsWithStats() ([]Domain, error)
 	AmendPolyclinicByID(id int, polyclinic Domain) error
 	RemovePolyclinicByID(id int) error
 }
@@ -19,4 +22,6 @@ type Repositories interface {
 	SelectDataByID(id int) (*Domain, error)
 	UpdateByID(id int, data Domain) error
 	DeleteByID(id int) error
+	CountDoctorByPolyclinic(polyclinic int) (int, error)
+	CountNurseByPolyclinic(polyclinic int) (int, error)
 }
